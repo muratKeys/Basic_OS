55aacb87eb811eb6a9fc7a23edd470e32f35699d
muratKeys <37161227+muratKeys@users.noreply.github.com>
2018-05-06 14:51:23 +0300

    Create boot.asm

---
+++ boot.asm
@@ -0,0 +1,28 @@
+Bits 16
+Org 0x7c00
+jmp main
+
+mesg db 'lahana',13,10,'written by muratKeys',0
+
+main:
+        mov     si, msg
+        call    print
+        mov     ah, 00h
+        int     16h
+        mov     ax, 0
+        int     19h
+
+print:
+        mov     ah, 0Eh
+
+.repeat:
+        lodsb
+        cmp     al, 0
+        je      .done
+        int     10h
+        jmp     .repeat
+
+.done:
+        ret
+
+times 510 -($ - $$) db 0
